f7130ffb32ce5dedafc3c5cbe0ff69443de725a9
chiefcadet <ravindersh@iitbhilai.ac.in>
2023-01-08 15:35:54 +0530

    Added custom input

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 let inputBtn=document.getElementById('input-btn')
+const myLeads=[]
+const inputEl=document.getElementById('input-el')
+const ulEl=document.getElementById('ul-el')
 
 inputBtn.addEventListener('click',function(){
-    
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value=''
 })
+
+
+function renderLeads(){
+    let listItems=''
+    myLeads.forEach(element =>{
+        listItems+=`
+        <li>
+            <a target='_blank' href='${element}'> 
+                ${element} 
+            </a>
+        </li>`
+    })
+    ulEl.innerHTML=listItems
+
+}
+renderLeads()
